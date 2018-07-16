5484fe2207ad76e24c1b83092379d8b5ba4d5738
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-07-16 20:42:11 +0200

    Opts

--- server/app.js
+++ server/app.js
@@ -57,11 +57,8 @@ var opts = {
     '192.168.0.1-200'
   ]
 };
-nmap.discover({
-  verbose: true
-}, function(err, report) {
+nmap.discover(opts, function(err, report) {
   if (err) throw new Error(err);
-
   for (let item in report) {
     console.log(JSON.stringify(report[item], null, 2));
   }
