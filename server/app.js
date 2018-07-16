ddeaafa1d64b8a6c5d2115d7e6869ed71786e389
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-07-16 21:01:57 +0200

    ARP

--- server/app.js
+++ server/app.js
@@ -57,6 +57,31 @@ var opts = {
     '192.168.0.1-200'
   ]
 };
+
+var fs = require('fs');
+
+fs.readFile('/proc/net/arp', function(err, data) {
+  if (!!err) return done(err, null);
+
+  var output = [];
+  var devices = data.toString().split('\n');
+  devices.splice(0, 1);
+
+  for (i = 0; i < devices.length; i++) {
+    var cols = devices[i].replace(/ [ ]*/g, ' ').split(' ');
+
+    if ((cols.length > 3) && (cols[0].length !== 0) && (cols[3].length !== 0) && cols[3] !== '00:00:00:00:00:00') {
+      output.push({
+        ip: cols[0],
+        mac: cols[3]
+      });
+    }
+  }
+
+  console.log(output);
+});
+
+/*
 nmap.discover({}, function(err, report) {
   console.log("\n\nREPORT\n\n\n")
   if (err) throw new Error(err);
@@ -67,7 +92,7 @@ nmap.discover({}, function(err, report) {
 setInterval(function() {
   var last_devices = [];
 }, 60000)
-
+*/
 
 
 
